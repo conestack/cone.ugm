51ab7562c0fdbe76b91984003f465f12de8e40bd
Attila Olah <attilaolah@gmail.com>
2011-02-04 16:30:12 +0100

    use pyramid_zcml

--- src/cone/ugm/run.py
+++ src/cone/ugm/run.py
@@ -1,3 +1,4 @@
+import pyramid_zcml
 from pyramid.configuration import Configurator
 from cone.ugm.model import get_root
 
@@ -6,6 +7,7 @@ def app(global_config, **settings):
     """
     zcml_file = settings.get('configure_zcml', 'configure.zcml')
     config = Configurator(root_factory=get_root, settings=settings)
+    config.include(pyramid_zcml)
     config.begin()
     config.load_zcml(zcml_file)
     config.end()
