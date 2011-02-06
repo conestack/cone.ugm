257ddd70194a89bae5bdd3577714c2fc707679bd
Robert Niederreiter <rnix@squarewave.at>
2011-02-06 23:27:22 +0100

    write app install path to os.environ and read in cone.ugm.model.utils

--- src/cone/ugm/run.py
+++ src/cone/ugm/run.py
@@ -1,12 +1,15 @@
 import pyramid_zcml
-from pyramid.configuration import Configurator
+from pyramid.config import Configurator
 from cone.ugm.model import get_root
 
 def app(global_config, **settings):
     """ This function returns a WSGI application.
     """
     zcml_file = settings.get('configure_zcml', 'configure.zcml')
-    config = Configurator(root_factory=get_root, settings=settings)
+    config = Configurator(
+        root_factory=get_root,
+        settings=settings,
+        autocommit=True)
     config.include(pyramid_zcml)
     config.begin()
     config.load_zcml(zcml_file)
