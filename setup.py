ef9eed465d8a87bf1675d59ab0b5ab0abeb13cc1
Florian Friesdorf <flo@chaoflow.net>
2011-03-03 01:24:56 +0100

    0.9b8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.0dev'
+version = '0.9b8'
 shortdesc = 'cone.ugm'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
