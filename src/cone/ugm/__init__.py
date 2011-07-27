90d2c1539c78a0a632f66e4c6d1ac3fa921828c2
Robert Niederreiter <rnix@squarewave.at>
2011-07-27 15:46:05 +0200

    add main hook for registering node.ext.ldap.ugm as authentication implementation

--- src/cone/ugm/__init__.py
+++ src/cone/ugm/__init__.py
@@ -7,6 +7,7 @@ from cone.ugm.model.settings import (
 )
 from cone.ugm.model.users import Users
 from cone.ugm.model.groups import Groups
+from node.ext.ldap.ugm import Ugm
 
 
 # custom UGM styles
@@ -40,3 +41,26 @@ cone.app.register_plugin('groups', Groups)
 # The node.ext.ugm implementation to use for user and group management
 # currently only LDAP
 backend = None
+
+
+# Flag whether LDAP ugm implementation should be used for authentication
+# XXX: move to cone.ldap later
+ldap_auth_enabled = False
+
+
+def initialize_auth_impl(config, global_config, local_config):
+    """Initialize LDAP based UGM implementation for cone.app as
+    authentication implementation.
+    """
+    ldap_auth_enabled = local_config.get('cone.ugm.ldap_auth')
+    if not ldap_auth_enabled:
+        return
+    root = cone.app.get_root()
+    props = root['settings']['ugm_server'].ldap_props
+    ucfg = root['settings']['ugm_users'].ldap_ucfg
+    gcfg = root['settings']['ugm_groups'].ldap_gcfg
+    ugm = Ugm(name='ldap_ugm', props=props, ucfg=ucfg, gcfg=gcfg)
+    cone.app.cfg.auth.append(ugm)
+
+
+cone.app.register_main_hook(initialize_auth_impl)
