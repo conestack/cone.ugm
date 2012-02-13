f8bca31dbc7cffd909e1c9291c76fe4daf74ea09
Robert Niederreiter <rnix@squarewave.at>
2012-02-13 10:14:20 +0100

    expiration widget wip

--- src/cone/ugm/browser/expires.py
+++ src/cone/ugm/browser/expires.py
@@ -5,19 +5,43 @@ from plumber import (
 )
 from pyramid.security import has_permission
 from yafowil.base import factory
+from yafowil.widget.datetime.widget import (
+    datetime_edit_renderer,
+    datetime_display_renderer,
+    datetime_extractor,
+)
 from cone.ugm.model.utils import ugm_users
 
 
 def expiration_extractor(widget, data):
-    pass
+    active = data.request.get('%s.active' % widget.name)
+    expires = datetime_extractor(widget, data)
+    return '12345'
 
 
 def expiration_edit_renderer(widget, data):
-    pass
+    tag = data.tag
+    active_attrs = dict()
+    active_attrs['type'] = 'checkbox'
+    active_attrs['name'] = '%s.active' % widget.name
+    active_attrs['value'] = '1'
+    active_attrs['checked'] = 'checked'
+    active = tag('input', **active_attrs)
+    expires = datetime_edit_renderer(widget, data)
+    return active + expires
 
 
 def expiration_display_renderer(widget, data):
-    pass
+    tag = data.tag
+    active_attrs = dict()
+    active_attrs['type'] = 'checkbox'
+    active_attrs['name'] = '%s.active' % widget.name
+    active_attrs['value'] = '1'
+    active_attrs['checked'] = 'checked'
+    active_attrs['disabled'] = 'disabled'
+    active = tag('input', **active_attrs)
+    expires = datetime_display_renderer(widget, data)
+    return active + expires
 
 
 factory.register(
@@ -25,6 +49,20 @@ factory.register(
     extractors=[expiration_extractor], 
     edit_renderers=[expiration_edit_renderer],
     display_renderers=[expiration_display_renderer])
+
+
+factory.doc['blueprint']['expiration'] = \
+"""Add-on blueprint UGM expiration widget. Utilizes yafowil.widget.datetime.
+"""
+
+factory.defaults['expiration.class'] = 'expiration'
+
+factory.defaults['expiration.datepicker_class'] = 'datepicker'
+
+factory.defaults['expiration.format'] = '%Y.%m.%d - %H:%M'
+factory.doc['props']['expiration.format'] = \
+"""Pattern accepted by ``datetime.strftime``.
+"""
 
 
 class ExpirationForm(Part):
@@ -43,6 +81,8 @@ class ExpirationForm(Part):
             name='expires',
             props={
                 'label': 'Expires',
+                'datepicker': True,
+                'locale': 'de',
             },
         )
         save_widget = self.form['save']
--- src/cone/ugm/browser/expires.rst
+++ src/cone/ugm/browser/expires.rst
@@ -4,7 +4,18 @@ Expiration widget
 ::
 
     >>> from yafowil.base import factory
-    >>> widget = factory('expiration')
+    >>> widget = factory(
+    ...     'expiration',
+    ...     name='expires',
+    ...     props={
+    ...         'datepicker': True,
+    ...         'locale': 'de',
+    ...     })
     
     >>> widget
-    <Widget object 'None' at ...>
+    <Widget object 'expires' at ...>
+    
+    >>> widget()
+    u'<input checked="checked" name="expires.active" type="checkbox" value="1" 
+    /><input class="datepicker expiration" id="input-expires" name="expires" 
+    size="10" type="text" />'
