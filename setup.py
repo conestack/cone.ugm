f81055821bdb47a74234645b70d3c766e04edf0d
Robert Niederreiter <office@squarewave.at>
2022-10-06 09:36:53 +0200

    Back to development: 1.0a6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_file(name):
         return f.read()
 
 
-version = '1.0a5'
+version = '1.0a6.dev0'
 shortdesc = 'User and group management'
 longdesc = '\n\n'.join([read_file(name) for name in [
     'README.rst',
