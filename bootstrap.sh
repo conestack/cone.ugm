536c59bb4e233e708b38fcb26d70eeabb4050193
Robert Niederreiter <rnix@squarewave.at>
2022-12-05 12:49:26 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,6 +10,6 @@ if [ "$PY" == "" ]; then
     PY=python2
 fi
 
-virtualenv --clear --no-site-packages -p $PY .
+virtualenv -p $PY .
 ./bin/pip install --upgrade pip setuptools zc.buildout
 ./bin/buildout -N
